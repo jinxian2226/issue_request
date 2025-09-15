427e0369a49b46781bc5989e5094712a39d76b5c
jinxian <xian8959@gmail.com>
2025-09-15 13:22:40 +0800

    123

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_barcode_scanner/flutter_barcode_scanner.dart';
 import 'package:provider/provider.dart';
 import '../services/parts_service.dart';
 import '../models/part.dart';
